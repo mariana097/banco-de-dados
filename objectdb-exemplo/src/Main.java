c8b945a4508e04b9ba8854535762f5d47b96111c
Mariana <92758993+mariana097@users.noreply.github.com>
2026-06-18 14:39:06 -0300

    Implement basic JPA persistence for Pessoa entity
    This code sets up an EntityManager to persist a 'Pessoa' object and retrieve a list of persisted 'Pessoa' objects.

--- objectdb-exemplo/src/Main.java
+++ objectdb-exemplo/src/Main.java
@@ -1,1 +1,37 @@
+import javax.persistence.*;
+import java.util.List;
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("objectdb:meubanco.odb");
+
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Pessoa p = new Pessoa("Mariana");
+
+        em.persist(p);
+
+        em.getTransaction().commit();
+
+        TypedQuery<Pessoa> consulta =
+                em.createQuery(
+                        "SELECT p FROM Pessoa p",
+                        Pessoa.class);
+
+        List<Pessoa> pessoas = consulta.getResultList();
+
+        System.out.println("Pessoas cadastradas:");
+
+        for (Pessoa pessoa : pessoas) {
+            System.out.println(pessoa.getId() + " - " + pessoa.getNome());
+        }
+
+        em.close();
+        emf.close();
+    }
+}
