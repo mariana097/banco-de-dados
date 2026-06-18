f411cc3e9d44e5cd30c95fc4ffe904c9ac352fc9
Mariana <92758993+mariana097@users.noreply.github.com>
2026-06-18 14:38:30 -0300

    Add Pessoa entity with id and nome fields

--- objectdb-exemplo/src/Pessoa.java
+++ objectdb-exemplo/src/Pessoa.java
@@ -1,1 +1,25 @@
+import javax.persistence.*;
 
+@Entity
+public class Pessoa {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String nome;
+
+    public Pessoa() {}
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
